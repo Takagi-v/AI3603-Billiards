c984feb3fce5fa29fb9183e363a540cbbe6b363d
Takagi <77490763+Takagi-v@users.noreply.github.com>
2026-01-04 21:49:47 +0800

    加入束搜索版本v5

--- evaluate.py
+++ evaluate.py
@@ -15,7 +15,7 @@ evaluate.py - Agent 评估脚本
 # 导入必要的模块
 from utils import set_random_seed
 from poolenv import PoolEnv
-from agents import BasicAgent, BasicAgentPro, NewAgent, NewAgentV2, NewAgentV3, NewAgentV4, AgentOthers
+from agents import BasicAgent, BasicAgentPro, NewAgent, NewAgentV2, NewAgentV3, NewAgentV4, NewAgentV5, AgentOthers
 
 # 设置随机种子，enable=True 时使用固定种子，enable=False 时使用完全随机
 # 根据需求，我们在这里统一设置随机种子，确保 agent 双方的全局击球扰动使用相同的随机状态
@@ -23,10 +23,10 @@ set_random_seed(enable=False, seed=42)
 
 env = PoolEnv()
 results = {'AGENT_A_WIN': 0, 'AGENT_B_WIN': 0, 'SAME': 0}
-n_games = 120  # 对战局数 自己测试时可以修改 扩充为120局为了减少随机带来的扰动
+n_games = 20  # 对战局数 自己测试时可以修改 扩充为120局为了减少随机带来的扰动
 
 # Agent A: 你的 NewAgent | Agent B: 别人的 OtherAgent
-agent_a, agent_b = NewAgentV4(), BasicAgentPro()
+agent_a, agent_b = NewAgentV5(), BasicAgentPro()
 
 players = [agent_a, agent_b]  # 用于切换先后手
 target_ball_choice = ['solid', 'solid', 'stripe', 'stripe']  # 轮换球型
